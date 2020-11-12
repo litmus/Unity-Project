00fad355781dcb641afd33ff93acf5d47fe2f650
Sam Holton <sholton311@gmail.com>
2020-11-12 11:42:18 -0500

    Merging Ed and Ash's game

---
+++ Assets/Scripts/Ed/Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * 5 * Time.deltaTime);
+
+
+        if(screenBounds.y < transform.position.y)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
